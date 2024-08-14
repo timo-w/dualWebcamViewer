655583156cf8bb8da86da5f55d1f851e26d2c2a0
Timothy Wang <2556936w@student.gla.ac.uk>
2024-08-14 10:02:53 +0100

    add python run script

---
+++ run.py
@@ -0,0 +1,26 @@
+import sys
+import time
+import threading
+import webbrowser
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+ip = "127.0.0.1"
+port = 9000
+directory = "assets/index.html"
+url = f"http://{ip}:{port}/{directory}"
+
+
+def start_server():
+    server_address = (ip, port)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    httpd.serve_forever()
+
+
+threading.Thread(target=start_server).start()
+webbrowser.open_new(url)
+
+while True:
+    try:
+        time.sleep(1)
+    except KeyboardInterrupt:
+        sys.exit(0)
